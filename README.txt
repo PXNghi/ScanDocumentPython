c5e432159fadbfddaa7984ed8eb1c2f9e1ffbc35
Augusta Phan <pxn.na6923@gmail.com>
2024-11-20 23:25:40 +0700

    init git
    first commit

---
+++ README.txt
@@ -0,0 +1,16 @@
+NHÓM 4 - HAI CHÚ ĐÓM CON
+
+Một số lưu ý trong chương trình:
+    1. Check đường dẫn gọi Pytesseract vì mỗi thiết bị có thể lưu đường dẫn ở vị trí khác nhau trên ổ đĩa.
+    2. Lưu ý khi mở file, mở đúng folder Nhom4-DuongNghi, không mở toàn bộ folder chứa cả file pdf báo cáo kèm theo vì 
+    khi đó sẽ dẫn đến việc sai đường dẫn gọi sample. Cụ thể để chạy được, đường dẫn phải là samples/ten_sample.jpg. Nếu mở folder 
+    ngoài kèm theo có file pdf báo cáo, đường dẫn sẽ bị đổi thành Nhom4-DuongNghi/samples/ten_sample.jpg => không chạy được ạ :<
+    3. Trong file có môi trường ảo đã có kèm theo file requirements.txt, file này bao gồm tất cả các thư viện sử dụng trong chương trình.
+    Để cài đặt, chuyển đổi qua môi trường ảo, nhập câu lệnh sau đây dưới terminal: pip install -r requirements.txt
+    4. Các file PDF trong VS bị mã hóa hoặc không xem được, để có thể xem file, chọn file cần xem -> chuột phải -> Reveal in File Explorer
+    và double click vào file cần xem.
+    5. Folder outputs là các folder chứa ảnh sau khi đã scan.
+
+Đây là một đồ án khá là khó khăn, nhóm em đã cố gắng hoàn thành một cách chỉnh chu nhất có thể nhưng vẫn khó tránh khỏi những sai sót
+và hạn chế vẫn còn tồn tại trong đồ án. Nhóm em rất mong thầy sẽ thông cảm vì các bước hạn chế mà tụi em chưa có thể linh động được. 
+Tụi em cảm ơn thầy rất nhiều ạ!
---
+++ main.py
@@ -0,0 +1,174 @@
+import cv2
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import pytesseract
+import transform
+import poly_editor as poly_f
+from matplotlib.patches import Polygon
+import img2pdf
+from PIL import Image
+from deep_translator import GoogleTranslator
+import tkinter as tk
+
+# Hay doc file README de biet them mot so luu y ve chuong trinh a
+
+SAMPLE_DIR = 'samples/'
+OUTPUT_DIR = 'outputs/'
+PDF_DIR = 'pdfs/'
+TXT_DIR = 'txts/'
+
+# Nhap hinh o day
+#################################################################
+image_path = 'sample1.jpg'
+#################################################################
+orig_image = cv2.imread(SAMPLE_DIR + image_path)
+
+def resizeImage(image, width = 500):
+    # get width and height of image
+    h,w,c = image.shape
+    # aspect ratio
+    height = int((h / w) * width)
+    size = (width, height)
+    image = cv2.resize(image, (width, height))
+    return image, size
+
+def interactive_get_contour(screenCnt, image):
+    poly = Polygon(screenCnt, animated=True, fill=False, color="yellow", linewidth=5)
+    fig, ax = plt.subplots()
+    ax.add_patch(poly)
+    ax.set_title(('Kéo các góc để định vị tài liệu.'))
+    p = poly_f.PolygonInteractor(ax, poly)
+    plt.imshow(image)
+    plt.show()
+
+    new_points = p.get_poly_points()[:4]
+    new_points = np.array([[p] for p in new_points], dtype = "int32")
+    return new_points.reshape(4, 2)
+
+def getContours(orig_image):
+    # image, size = resizeImage(orig_image.copy())
+    image = orig_image
+    size = orig_image.shape # h w c
+    detail_img = cv2.detailEnhance(image, sigma_s=20, sigma_r=0.15)
+    gray_img = cv2.cvtColor(detail_img, cv2.COLOR_BGR2GRAY)
+    _, threshold= cv2.threshold(gray_img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    blur = cv2.GaussianBlur(threshold, (5,5), 0)
+    edge_img = cv2.Canny(blur, 0, 50)
+    # morphological transform
+    kernel = np.ones((5, 5), np.uint8)
+    # increase the edges with thicker line is 1
+    dilate = cv2.dilate(edge_img, kernel, iterations=1)
+    # close the gap between each item
+    closing = cv2.morphologyEx(dilate, cv2.MORPH_CLOSE, kernel)
+
+    approx_contours = []
+    # find the contours
+    contours, _ = cv2.findContours(closing, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
+    for contour in contours:
+        approx = cv2.approxPolyDP(contour, 80, True)
+
+        if len(approx) == 4:
+            approx_contours.append(approx)
+            break
+    
+    if not approx_contours:
+            TOP_RIGHT = (size[1], 0)
+            BOTTOM_RIGHT = (size[1], size[0])
+            BOTTOM_LEFT = (0, size[0])
+            TOP_LEFT = (0, 0)
+            screenCnt = np.array([[TOP_RIGHT], [BOTTOM_RIGHT], [BOTTOM_LEFT], [TOP_LEFT]])
+
+    else:
+        screenCnt = max(approx_contours, key=cv2.contourArea)
+    
+    return screenCnt.reshape(4,2)
+
+def processContours(orig_image):
+    imageCnt = getContours(orig_image)
+    # img = resizeImage(orig_image)[0]
+    img = orig_image
+    screenCnt = interactive_get_contour(screenCnt=imageCnt, image=img)
+    wrap_image = transform.four_point_transform(image=img, pts=screenCnt)
+    # convert the warped image to grayscale
+    gray = cv2.cvtColor(wrap_image, cv2.COLOR_BGR2GRAY)
+    # sharpen image
+    sharpen = cv2.GaussianBlur(gray, (0,0), 3)
+    sharpen = cv2.addWeighted(gray, 1.5, sharpen, -0.5, 0)
+    # apply adaptive threshold to get black and white effect
+    thresh = cv2.adaptiveThreshold(sharpen, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 21, 15)
+    # save the transformed image
+    save_file_to_dir(image_path, thresh)
+
+    return thresh
+
+def save_file_to_dir(image_path, image):
+    basename = os.path.basename(image_path)
+    cv2.imwrite(OUTPUT_DIR + basename, image)
+    cv2.imwrite(PDF_DIR + basename, image)
+    print("Đang xử lý " + image_path + "...")
+
+def replace_extension(text, ext):
+    text_index = text.find('.')
+    text = text[:text_index + 1] + ext
+    return text
+
+def check_file_existed(dir, image_path, ext):
+    image_path = replace_extension(image_path, ext)
+    if os.path.isfile(dir + image_path):
+        os.remove(dir + image_path)
+
+def img_to_pdf(image_path):
+    image = Image.open(image_path)
+    pdf_bytes = img2pdf.convert(image.filename)
+    file = open(image_path, 'wb')
+    file.write(pdf_bytes)
+    image.close()
+    file.close()
+    p = replace_extension(image_path, 'pdf')
+    os.rename(image_path, p)
+    print("File PDF đã được tạo thành công. Mời kiểm tra thư mục pdfs.")
+    return p
+
+def write_to_txt(text, image_path):
+    display_text_in_window(text=text, title='Original') 
+    text = translate_text(text)
+    display_text_in_window(text=text, title='Translated') 
+    file = open(TXT_DIR + image_path, "x", encoding='utf-8')
+    file.write(text)
+    file.close()
+    image_path = TXT_DIR + image_path
+    p = replace_extension(image_path, 'txt')
+    os.rename(image_path, p)
+
+def translate_text(text):
+    translated = GoogleTranslator(source='auto', target='vi').translate(text)
+    return translated
+
+def itotext(image, image_path):
+    pytesseract.pytesseract.tesseract_cmd = 'c:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
+    text = pytesseract.image_to_string(image)
+    print(text)
+    write_to_txt(text=text, image_path=image_path)
+    img_to_pdf(PDF_DIR + image_path)
+    return text
+
+def display_text_in_window(text, title):
+    root = tk.Tk()
+    root.title(title)
+    text_widget = tk.Text(root)
+    text_widget.insert("1.0", text)
+    text_widget.pack()
+    root.mainloop()
+
+def main():
+    check_file_existed(dir=PDF_DIR, image_path=image_path, ext='pdf')
+    check_file_existed(dir=TXT_DIR, image_path=image_path, ext='txt')
+    wrap_image = processContours(orig_image)
+    itotext(wrap_image, image_path)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__  == '__main__':
+    main()
